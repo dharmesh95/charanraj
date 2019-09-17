82ff8046336bac55cd670372a06e5c6d897b37b8
dharmesh95 <95dharmesh@gmail.com>
2019-09-16 21:40:49 -0400

    url fowarding test

--- src/main/java/com/dharmesh/charanraj/config/MyWebMvcConfig.java
+++ src/main/java/com/dharmesh/charanraj/config/MyWebMvcConfig.java
@@ -6,17 +6,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MyWebMvcConfig implements WebMvcConfigurer {
-	/*
-	 * @Override public void addResourceHandlers(ResourceHandlerRegistry registry) {
-	 * registry.addResourceHandler("/**").addResourceLocations(
-	 * "file:/here/some/path"); }
-	 */
 
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/").setViewName("forward:/index.html");
 		registry.addViewController("/login").setViewName("forward:/index.html");
 		registry.addViewController("/home").setViewName("forward:/index.html");
-		registry.addViewController("/home/**").setViewName("forward:/home/index.html");
+		registry.addViewController("/home/**").setViewName("forward:/index.html");
 	}
 }
