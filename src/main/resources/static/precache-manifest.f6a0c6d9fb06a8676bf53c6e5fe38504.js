fc0bf12daa308f805ab083c187f897389f64f0a1
dharmesh95 <95dharmesh@gmail.com>
2019-10-15 04:13:59 -0400

    date changes

---
+++ src/main/resources/static/precache-manifest.f6a0c6d9fb06a8676bf53c6e5fe38504.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8254839168d581f7ba8c3c6d826918ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb7d40b2d757c9ce40ee",
+    "url": "/static/css/main.d3dc802f.chunk.css"
+  },
+  {
+    "revision": "4816d3f250264c662fc2",
+    "url": "/static/js/2.488a4455.chunk.js"
+  },
+  {
+    "revision": "bb7d40b2d757c9ce40ee",
+    "url": "/static/js/main.adb3c200.chunk.js"
+  },
+  {
+    "revision": "de50ad6a532eecb88e9e",
+    "url": "/static/js/runtime~main.6f96a955.js"
+  }
+]);
