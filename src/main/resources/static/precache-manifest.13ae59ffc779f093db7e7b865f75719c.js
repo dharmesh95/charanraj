9212320d4cb7f9efe20f21f5ec11e9b0892be22c
Dharmesh Patel <95dharmesh@gmail.com>
2020-12-29 18:11:50 -0500

    Auto Committed with message: db updated

---
+++ src/main/resources/static/precache-manifest.13ae59ffc779f093db7e7b865f75719c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4b348c8e8a3e7ae39163197b58724d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "627a8df8bcd373a93fd8",
+    "url": "/static/css/main.a5d9470b.chunk.css"
+  },
+  {
+    "revision": "3bb043d82cee69844eaf",
+    "url": "/static/js/0.7384717f.chunk.js"
+  },
+  {
+    "revision": "14c3158bb8bb975b5f3a",
+    "url": "/static/js/10.266667bb.chunk.js"
+  },
+  {
+    "revision": "66804dba07692d7dd333",
+    "url": "/static/js/11.3123be4b.chunk.js"
+  },
+  {
+    "revision": "9d5b522ab4f59ddfe6d8",
+    "url": "/static/js/12.a3dabfea.chunk.js"
+  },
+  {
+    "revision": "65bf52a9d325d71381a8",
+    "url": "/static/js/3.487e06e8.chunk.js"
+  },
+  {
+    "revision": "e4cb1eae69651c7c2861",
+    "url": "/static/js/4.faeadef0.chunk.js"
+  },
+  {
+    "revision": "321bbc7d437597def870b0dc001f4797",
+    "url": "/static/js/4.faeadef0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30d5026b8bff16ce9955",
+    "url": "/static/js/5.0dc3d9a3.chunk.js"
+  },
+  {
+    "revision": "aa61f3c442d5260d29d7",
+    "url": "/static/js/6.5f373be2.chunk.js"
+  },
+  {
+    "revision": "bb8bb029739e2719de74",
+    "url": "/static/js/7.98e4d85b.chunk.js"
+  },
+  {
+    "revision": "e13ec8c4bb2297a779f4",
+    "url": "/static/js/8.48eff50a.chunk.js"
+  },
+  {
+    "revision": "e9b29285ce2d86cbc856",
+    "url": "/static/js/9.22189804.chunk.js"
+  },
+  {
+    "revision": "627a8df8bcd373a93fd8",
+    "url": "/static/js/main.88353b67.chunk.js"
+  },
+  {
+    "revision": "203fe801889630adaa47",
+    "url": "/static/js/runtime-main.7b0e972c.js"
+  }
+]);
